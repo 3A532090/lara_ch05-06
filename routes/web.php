7ab649b75beb6aa5d71880afb087d4905fc3cb7b
3A532090 <raxttc@gmail.com>
2018-11-01 20:42:56 +0800

    練習1~7修正&建立HomeController

--- routes/web.php
+++ routes/web.php
@@ -33,14 +33,14 @@ Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject
 
 /*正規表達式限制參數*/
 Route::get('student/{student_no}',function ($student_no){
-    return "學號：".$student_no})->where(['student_no'=>'s[0-9]{10}']);
+    return "學號：".$student_no;})->where(['student_no'=>'s[0-9]{10}']);
 Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject = null){
     return "學號：".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";})->where(['student_no'=>'s[0-9]{10}','subject'=>'chinese|english|math']);
 Route::pattern('student_no','s[0-9]{10}');
 Route::get('student/{student_no}',function ($student_no){
     return "學號：".$student_no;
     });
-    Roure::get('student/{student_no}/score/{subject?}',function($student_no,$subject = null){
+    Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject = null){
         return "學號：".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
     })->where(['subject'=>'chinese|english|math']);
 
