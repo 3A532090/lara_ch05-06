e77f0c70294b47f76a109d711f19bb09a5115133
3A532090 <raxttc@gmail.com>
2018-11-01 20:26:41 +0800

    練習六

--- routes/web.php
+++ routes/web.php
@@ -15,30 +15,41 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*基礎路由參數*/
 Route::get('student/{student_no}',function ($student_no){
     return "學號：".$student_no;
 });
 Route::get('student/{student_no}/score',function ($student_no){
     return "學號：".$student_no."的所有成績";
 });
-
 Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject){
     return "學號：".$student_no."的".$subject."成績";
 });
 
+/*選擇性路由參數*/
 Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject = null){
     return "學號：".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
 });
 
+/*正規表達式限制參數*/
 Route::get('student/{student_no}',function ($student_no){
     return "學號：".$student_no})->where(['student_no'=>'s[0-9]{10}']);
 Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject = null){
     return "學號：".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";})->where(['student_no'=>'s[0-9]{10}','subject'=>'chinese|english|math']);
-
 Route::pattern('student_no','s[0-9]{10}');
 Route::get('student/{student_no}',function ($student_no){
     return "學號：".$student_no;
     });
     Roure::get('student/{student_no}/score/{subject?}',function($student_no,$subject = null){
+        return "學號：".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";})->where(['subject'=>'chinese|english|math']);
+
+    /*路由群組*/
+Route::pattern('student_no','s[0-9]{10}');
+Route::group(['prefix'=>'student'],function(){
+    Route::get('{student_no}', function ($student_no) {
+        return "學號：" . $student_no;
+    });
+    Route::get('{student_no}/score/{subject?}',function($student_no,$subject = null){
         return "學號：".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
-    })->where(['subject'=>'chinese|english|math']);
+    })->where(['subject'=>'(chinese|english|math)']);
+});
